07eca40f896a85dafde7273e8e89afc41025bb97
Max <BiginMS@stud.kai.ru>
2021-07-20 17:43:20 +0300

    auto start project Django

---
+++ start-project.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+python_interpreter=""
+project_name=""
+app_name=""
+
+read -p "Python interpreter: " python_interpreter
+read -p "Project name: " project_name
+read -p "App name: " app_name
+
+
+`$python_interpreter -m venv env`
+source env/bin/activate
+
+pip install -U pip
+pip install -r requirements.txt
+
+`django-admin startproject $project_name`
+
+`python manage.py startapp $app_name`
+
+python manage.py runserver
