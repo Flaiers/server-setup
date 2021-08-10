7e5adc20f1afcd100ac8356c20434d57b369c120
Max <bigin.maks@gmail.com>
2021-08-10 21:24:43 +0300

    add mysql to settings and description

--- settings/production.py
+++ settings/production.py
@@ -4,6 +4,8 @@ from .base import env
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
+
+    # Use PostgreSQL: add to requirements psycopg2
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': env('POSTGRES_DB'),
@@ -11,5 +13,15 @@ DATABASES = {
         'PASSWORD': env('POSTGRES_PASSWORD'),
         'HOST': env('HOST'),
         'PORT': 5432,
+    },
+
+    # Use MySQL: add to requirements mysqlclient
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': env('MYSQL_DATABASE'),
+        'USER': env('MYSQL_USER'),
+        'PASSWORD': env('MYSQL_PASS'),
+        'HOST': env('HOST'),
+        'PORT': 3306,
     }
 }
