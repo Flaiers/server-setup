cf573f5873892661459335d73aefb65dbe36b9a2
Max <bigin.maks@gmail.com>
2021-08-11 20:55:23 +0300

    comment

--- build.sh
+++ build.sh
@@ -6,6 +6,7 @@ read -p "----------------------
 | Python interpreter |
 ----------------------
 Default: /usr/bin/python3
+(Press: Enter for choose default)
 If you wont to change, write: " python_interpreter
 
 read -p "
@@ -13,6 +14,7 @@ read -p "
 | First app name |
 ------------------
 Default: core
+(Press: Enter for choose default)
 If you wont to change, write: " first_app_name
 
 
